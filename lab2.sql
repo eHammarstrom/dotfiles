ed7d6a2fd05be602b3307e4736c0af03d0280b47
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2016-02-02 19:56:29 +0100

    working.

--- lab2.sql
+++ lab2.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS performances;
 DROP TABLE IF EXISTS reservations;
+DROP TABLE IF EXISTS performances;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS theaters;
@@ -25,20 +25,25 @@ CREATE TABLE theaters (
 
 CREATE TABLE performances (
   moviename VARCHAR(25),
-  pdate DATE,
+  date DATE,
   theatername VARCHAR(25),
-  PRIMARY KEY(moviename, theatername),
+  PRIMARY KEY(moviename, date),
   FOREIGN KEY(moviename) REFERENCES movies(name) ON DELETE CASCADE,
   FOREIGN KEY(theatername) REFERENCES theaters(name) ON DELETE CASCADE
 );
 
 CREATE TABLE reservations (
-  resnbr INT NOT NULL UNIQUE,
-  rdate DATE,
+  resnbr INTEGER NOT NULL UNIQUE,
+  date DATE,
   moviename VARCHAR(25),
   username VARCHAR(25),
   PRIMARY KEY(resnbr),
-  FOREIGN KEY(rdate) REFERENCES performances(pdate) ON DELETE CASCADE,
-  FOREIGN KEY(moviename) REFERENCES performances(moviename) ON DELETE CASCADE,
+  FOREIGN KEY(moviename, date) REFERENCES performances(moviename, date) ON DELETE CASCADE,
   FOREIGN KEY(username) REFERENCES users(username) ON DELETE CASCADE
 );
+
+DESCRIBE users;
+DESCRIBE movies;
+DESCRIBE theaters;
+DESCRIBE performances;
+DESCRIBE reservations;
