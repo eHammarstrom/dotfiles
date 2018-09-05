7ca365732fb0c3641b6569861ee1902258b5a243
Emil Hammarström <emil.a.hammarstrom@gmail.com>
2018-09-05 11:15:19 +0200

    oi

---
+++ .emacs.d/init.el
@@ -0,0 +1,105 @@
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 4))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(package-initialize)
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+					; packages
+(use-package evil)
+(use-package evil-surround)
+(use-package labburn-theme)
+(use-package company
+  :bind (("<tab>" . company-complete)))
+(use-package racket-mode)
+(use-package helm)
+(use-package lsp-mode)
+(use-package lsp-ui)
+(use-package lsp-haskell)
+(use-package helm-ls-git)
+(use-package which-key)
+
+; enable all things
+(evil-mode 1)
+(global-evil-surround-mode 1)
+(global-company-mode t)
+(helm-mode 1)
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode t)
+(which-key-mode)
+
+(add-hook 'lsp-mode-hook 'lsp-ui-mode)
+(add-hook 'haskell-mode-hook #'lsp-haskell-enable)
+(add-hook 'haskell-mode-hook 'flycheck-mode)
+
+(global-set-key (kbd "C-SPC")			'helm-M-x)
+(global-set-key (kbd "C-a")			'align-regexp)
+
+(define-key evil-motion-state-map (kbd "C-f")   'helm-ls-git-ls)
+
+(define-key evil-normal-state-map (kbd "H")	'evil-first-non-blank)
+(define-key evil-normal-state-map (kbd "S")	'evil-end-of-line)
+
+(define-key evil-insert-state-map (kbd "C-c")	'evil-force-normal-state)
+
+(define-key evil-motion-state-map (kbd "C-e")
+  (lambda () (interactive) (evil-scroll-down 0)))
+(define-key evil-motion-state-map (kbd "C-u")
+  (lambda () (interactive) (evil-scroll-up 0)))
+(define-key evil-normal-state-map (kbd "C-p")	'helm-mini)
+
+(global-set-key (kbd "C-S-N")			'evil-window-up)
+(global-set-key (kbd "C-S-T")			'evil-window-down)
+(global-set-key (kbd "C-S-H")			'evil-window-left)
+(global-set-key (kbd "C-S-S")			'evil-window-right)
+(global-set-key (kbd "C-\\")			'split-window-horizontally)
+(global-set-key (kbd "C-\-")			'split-window-vertically)
+
+(define-key evil-visual-state-map "s"		'evil-substitute)
+(define-key evil-visual-state-map "S"		'evil-surround-region)
+(define-key evil-visual-state-map "h"		'evil-backward-char)
+(define-key evil-visual-state-map "t"		'evil-next-line)
+(define-key evil-visual-state-map "n"		'evil-previous-line)
+(define-key evil-visual-state-map "s"		'evil-forward-char)
+
+(define-key evil-normal-state-map (kbd "h")	'backward-char)
+(define-key evil-normal-state-map (kbd "t")	'evil-next-line)
+(define-key evil-normal-state-map (kbd "n")	'previous-line)
+(define-key evil-normal-state-map (kbd "s")	'forward-char)
+
+(define-key evil-normal-state-map (kbd "<return>")
+  (lambda () (interactive)
+    (evil-open-below 1)
+    (evil-force-normal-state)
+    (evil-previous-line 1)))
+
+(define-key evil-normal-state-map (kbd "S-<return>")
+  (lambda () (interactive)
+    (evil-open-above 1)
+    (evil-force-normal-state)
+    (evil-next-line 1)))
+
+(define-key evil-normal-state-map
+  "T" #'(lambda () (interactive)
+          "join this line at the end of the line below"
+          (join-line 1)))
+
+(setq visible-bell 1)
+(setq inhibit-startup-screen t)
+(setq jit-lock-defer-time 0)
+(setq fast-but-imprecise-scrolling t)
+
+(setq scroll-conservatively 10)
+(setq scroll-margin 7)
+
+(set-frame-font "Hasklig-16" nil t)
+(tool-bar-mode -1)
