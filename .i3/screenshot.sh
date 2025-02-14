684de1521905670155095d7ee420033c459c25ce
Emil Hammarström <emil.hammarstrom1@assaabloy.com>
2025-02-14 14:20:48 +0100

    Sway compatible config
    Signed-off-by: Emil Hammarström <emil.hammarstrom1@assaabloy.com>

--- .i3/screenshot.sh
+++ .i3/screenshot.sh
@@ -4,6 +4,10 @@ mkdir -p $HOME/Pictures/Screenshots
 
 OUTPUT=$HOME/Pictures/Screenshots/$(date +%Y%m%d%H%M%S).png
 
-scrot -s $OUTPUT
+if [ $XDG_SESSION_TYPE = "x11" ]; then
+    scrot -s $OUTPUT
+else
+    grim -g "$(slurp)" $OUTPUT
+fi
 
 xclip -i $OUTPUT -t image/png -selection clipboard
